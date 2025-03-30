de86ccf4c8726f74ab1b3ab850bfe1037163a3d2
kevin <desk2003@USherbrooke.ca>
2025-03-30 14:09:26 -0400

    Combat dice mistake

--- Source/Risky/Private/Character/BaseCharacter.cpp
+++ Source/Risky/Private/Character/BaseCharacter.cpp
@@ -34,15 +34,34 @@ bool ABaseCharacter::CombatRegion(ARegion* ownRegion, ARegion* enemyRegion, int3
 void ABaseCharacter::CombatRoll(ARegion* ownRegion, ARegion* enemyRegion, int32 attackingUnits)
 {
 	TArray<int32> attackingDice;
-	attackingDice.Init(FMath::RandRange(1, 6), attackingUnits);
+	attackingDice.Reserve(attackingUnits);
+	for (size_t i = 0; i < attackingUnits; i++)
+	{
+		attackingDice.Add(FMath::RandRange(1, 6));
+	}
 	attackingDice.Sort();
 
 	TArray<int32> defendingDice;
 	int32 defenders = FMath::Min(2, enemyRegion->GetUnits());
-	defendingDice.Init(FMath::RandRange(1, 6), defenders);
+	defendingDice.Reserve(defenders);
+	for (size_t i = 0; i < defenders; i++)
+	{
+		defendingDice.Add(FMath::RandRange(1, 6));
+	}
 	defendingDice.Sort();
 
 	int32 maxIteration = FMath::Min(defenders, attackingUnits) - 1;
+
+	for (auto dice : attackingDice)
+	{
+		FString Message = FString::Format(TEXT("Attacking: {0}"), { dice });
+		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Blue, Message);
+	}
+	for (auto dice : defendingDice)
+	{
+		FString Message = FString::Format(TEXT("Defending: {0}"), { dice });
+		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Blue, Message);
+	}
 	for (int32 i = 0; i <= maxIteration; ++i)
 	{
 		FString Message = FString::Format(TEXT("AttackingDice: {0} DefendingDice: {1}"), { attackingDice[attackingDice.Num() - 1 - i], defendingDice[defendingDice.Num() - 1 - i] });
